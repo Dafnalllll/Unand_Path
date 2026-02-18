0c274e137f2e58ba48f33ef12cb72f66db891b33
Rizki Dafa Naldi <rizkidafa0404@gmail.com>
2026-02-18 16:17:00 +0700

    r2

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -14,6 +14,6 @@ test('new users can register', function () {
         'password_confirmation' => 'password',
     ]);
 
-    $this->assertAuthenticated();
-    $response->assertRedirect(route('dashboard', absolute: false));
+    $this->assertGuest(); // User belum login
+    $response->assertRedirect('/login'); // Redirect ke halaman login
 });
